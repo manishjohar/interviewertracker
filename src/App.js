6de5a6b35ee1c39e61fed3806faef2b94e3d3386
manishjohar <manish.johar@globallogic.com>
2018-03-15 16:52:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import 'babel-polyfill';
+import React, { Component } from 'react';
+import { Admin, Delete, Resource } from 'admin-on-rest';
+
+import './App.css';
+
+import authClient from './authClient';
+import sagas from './sagas';
+import themeReducer from './themeReducer';
+import Login from './Login';
+import Layout from './Layout';
+import Menu from './Menu';
+import { Dashboard } from './dashboard';
+import customRoutes from './routes';
+import translations from './i18n';
+
+import { VisitorList, VisitorEdit, VisitorDelete, VisitorIcon } from './visitors';
+import { CommandList, CommandEdit, CommandIcon } from './commands';
+
+import { ReviewList, ReviewEdit, ReviewIcon } from './reviews';
+
+import restClient from './restClient';
+import fakeRestServer from './restServer';
+
+class App extends Component {
+    componentWillMount() {
+        this.restoreFetch = fakeRestServer();
+    }
+
+    componentWillUnmount() {
+        this.restoreFetch();
+    }
+
+    render() {
+        return (
+            <Admin
+                title="Interviewer Tracker"
+                restClient={restClient}
+                customReducers={{ theme: themeReducer }}
+                customSagas={sagas}
+                customRoutes={customRoutes}
+                authClient={authClient}
+                dashboard={Dashboard}
+                loginPage={Login}
+                appLayout={Layout}
+                menu={Menu}
+                messages={translations}
+            >
+                <Resource name="customers" list={VisitorList} edit={VisitorEdit} remove={VisitorDelete} icon={VisitorIcon} />
+                <Resource name="commands" list={CommandList} edit={CommandEdit} remove={Delete} icon={CommandIcon} options={{ label: 'Schedule Interview' }}/>
+                    <Resource name="reviews" list={ReviewList} edit={ReviewEdit} icon={ReviewIcon} />
+            </Admin>
+        );
+    }
+}
+
+export default App;
---
+++ src/i18n/en.js
@@ -0,0 +1,90 @@
+export default {
+    pos: {
+        search: 'Search',
+        configuration: 'Configuration',
+        language: 'Language',
+        theme: {
+            name: 'Theme',
+            light: 'Light',
+            dark: 'Dark',
+        },
+        dashboard: {
+            monthly_revenue: 'Monthly Revenue',
+            new_orders: 'New Orders',
+            pending_reviews: 'Pending Reviews',
+            new_customers: 'New Customers',
+            pending_orders: 'Pending Orders',
+            order: {
+                items: 'by %{customer_name}, one item |||| by %{customer_name}, %{nb_items} items',
+            },
+            welcome: {
+                title: 'Welcome to Interviewer Tracker',
+                subtitle: 'This is the admin of an imaginary poster shop. Fell free to explore and modify the data - it\'s local to your computer, and will reset each time you reload.',
+            },
+        },
+    },
+    resources: {
+        customers: {
+            name: 'Canditate |||| Canditates',
+            fields: {
+                commands: 'Orders',
+                groups: 'Segments',
+                last_seen_gte: 'Visited Since',
+                name: 'Name',
+            },
+            tabs: {
+                identity: 'Identity',
+                address: 'Address',
+                orders: 'Orders',
+                reviews: 'Reviews',
+                stats: 'Stats',
+            },
+            page: {
+                delete: 'Delete Customer',
+            },
+
+        },
+        commands: {
+            name: 'Schedule Interview',
+            fields: {
+                basket: {
+                    delivery: 'Delivery',
+                    reference: 'Reference',
+                    quantity: 'Quantity',
+                    sum: 'Sum',
+                    tax_rate: 'Tax Rate',
+                    total: 'Total',
+                    unit_price: 'Unit Price',
+                },
+                customer_id: 'Customer',
+                date_gte: 'Passed Since',
+                date_lte: 'Passed Before',
+                total_gte: 'Min amount',
+            },
+        },
+        
+        reviews: {
+            name: 'Interviewer |||| Interviewers',
+            fields: {
+                customer_id: 'Customer',
+                command_id: 'Order',
+                product_id: 'Product',
+                date_gte: 'Posted since',
+                date_lte: 'Posted before',
+                date: 'Date',
+                comment: 'Comment',
+                rating: 'Rating',
+            },
+            action: {
+                accept: 'Accept',
+                reject: 'Reject',
+            },
+            notification: {
+                approved_success: 'Review approved',
+                approved_error: 'Error: Review not approved',
+                rejected_success: 'Review rejected',
+                rejected_error: 'Error: Review not rejected',
+            },
+        },
+    },
+};
